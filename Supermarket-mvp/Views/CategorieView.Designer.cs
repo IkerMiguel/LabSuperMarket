3893f37b31185c84c35040e59de5c7f8da6347d2
IkerMiguel <ikmiguel03@gmail.com>
2024-10-09 00:16:28 -0500

    Add Categorie View

--- Supermarket-mvp/Views/CategorieView.Designer.cs
+++ Supermarket-mvp/Views/CategorieView.Designer.cs
@@ -28,12 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CategorieView";
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            tabControl1 = new TabControl();
+            tabPageCategorieList = new TabPage();
+            tabPageCategorieDetail = new TabPage();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            BtnSearch = new Button();
+            BtnNew = new Button();
+            BtnEdit = new Button();
+            BtnDelete = new Button();
+            BtnClose = new Button();
+            dataGridView1 = new DataGridView();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            BtnSave = new Button();
+            BtnCancel = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPageCategorieList.SuspendLayout();
+            tabPageCategorieDetail.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 122);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.sell;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 95);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 32F, FontStyle.Bold);
+            label1.Location = new Point(133, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(220, 59);
+            label1.TabIndex = 1;
+            label1.Text = "Categorie";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageCategorieList);
+            tabControl1.Controls.Add(tabPageCategorieDetail);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 122);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 328);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPageCategorieList
+            // 
+            tabPageCategorieList.Controls.Add(dataGridView1);
+            tabPageCategorieList.Controls.Add(BtnClose);
+            tabPageCategorieList.Controls.Add(BtnDelete);
+            tabPageCategorieList.Controls.Add(BtnEdit);
+            tabPageCategorieList.Controls.Add(BtnNew);
+            tabPageCategorieList.Controls.Add(BtnSearch);
+            tabPageCategorieList.Controls.Add(label3);
+            tabPageCategorieList.Controls.Add(textBox1);
+            tabPageCategorieList.Controls.Add(label2);
+            tabPageCategorieList.Location = new Point(4, 24);
+            tabPageCategorieList.Name = "tabPageCategorieList";
+            tabPageCategorieList.Padding = new Padding(3);
+            tabPageCategorieList.Size = new Size(792, 300);
+            tabPageCategorieList.TabIndex = 0;
+            tabPageCategorieList.Text = "Categorie List";
+            tabPageCategorieList.UseVisualStyleBackColor = true;
+            // 
+            // tabPageCategorieDetail
+            // 
+            tabPageCategorieDetail.Controls.Add(BtnCancel);
+            tabPageCategorieDetail.Controls.Add(BtnSave);
+            tabPageCategorieDetail.Controls.Add(label6);
+            tabPageCategorieDetail.Controls.Add(textBox4);
+            tabPageCategorieDetail.Controls.Add(textBox3);
+            tabPageCategorieDetail.Controls.Add(label5);
+            tabPageCategorieDetail.Controls.Add(textBox2);
+            tabPageCategorieDetail.Controls.Add(label4);
+            tabPageCategorieDetail.Location = new Point(4, 24);
+            tabPageCategorieDetail.Name = "tabPageCategorieDetail";
+            tabPageCategorieDetail.Padding = new Padding(3);
+            tabPageCategorieDetail.Size = new Size(792, 300);
+            tabPageCategorieDetail.TabIndex = 1;
+            tabPageCategorieDetail.Text = "Categorie Detail";
+            tabPageCategorieDetail.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Search categorie";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 30);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Data to search";
+            textBox1.Size = new Size(539, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 74);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 2;
+            // 
+            // BtnSearch
+            // 
+            BtnSearch.Image = Properties.Resources.search_small;
+            BtnSearch.Location = new Point(568, 15);
+            BtnSearch.Name = "BtnSearch";
+            BtnSearch.Size = new Size(50, 50);
+            BtnSearch.TabIndex = 3;
+            BtnSearch.UseVisualStyleBackColor = true;
+            // 
+            // BtnNew
+            // 
+            BtnNew.Image = Properties.Resources._new;
+            BtnNew.Location = new Point(626, 74);
+            BtnNew.Name = "BtnNew";
+            BtnNew.Size = new Size(160, 50);
+            BtnNew.TabIndex = 4;
+            BtnNew.UseVisualStyleBackColor = true;
+            // 
+            // BtnEdit
+            // 
+            BtnEdit.Image = Properties.Resources.edit;
+            BtnEdit.Location = new Point(626, 130);
+            BtnEdit.Name = "BtnEdit";
+            BtnEdit.Size = new Size(160, 50);
+            BtnEdit.TabIndex = 5;
+            BtnEdit.UseVisualStyleBackColor = true;
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.Image = Properties.Resources.delete;
+            BtnDelete.Location = new Point(626, 186);
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(160, 50);
+            BtnDelete.TabIndex = 6;
+            BtnDelete.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            BtnClose.Image = Properties.Resources.cerrar;
+            BtnClose.Location = new Point(626, 242);
+            BtnClose.Name = "BtnClose";
+            BtnClose.Size = new Size(160, 50);
+            BtnClose.TabIndex = 7;
+            BtnClose.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 74);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(606, 218);
+            dataGridView1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 12);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Categorie Id";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 30);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(236, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 69);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Categorie name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 87);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(236, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 148);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(236, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 130);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Description";
+            // 
+            // BtnSave
+            // 
+            BtnSave.Image = Properties.Resources.save;
+            BtnSave.Location = new Point(28, 196);
+            BtnSave.Name = "BtnSave";
+            BtnSave.Size = new Size(75, 50);
+            BtnSave.TabIndex = 7;
+            BtnSave.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            BtnCancel.Image = Properties.Resources.cancel;
+            BtnCancel.Location = new Point(129, 196);
+            BtnCancel.Name = "BtnCancel";
+            BtnCancel.Size = new Size(75, 50);
+            BtnCancel.TabIndex = 8;
+            BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // CategorieView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Controls.Add(panel1);
+            Name = "CategorieView";
+            Text = "CategorieView";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPageCategorieList.ResumeLayout(false);
+            tabPageCategorieList.PerformLayout();
+            tabPageCategorieDetail.ResumeLayout(false);
+            tabPageCategorieDetail.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TabControl tabControl1;
+        private TabPage tabPageCategorieList;
+        private TabPage tabPageCategorieDetail;
+        private Button BtnClose;
+        private Button BtnDelete;
+        private Button BtnEdit;
+        private Button BtnNew;
+        private Button BtnSearch;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private Label label6;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox2;
+        private Label label4;
+        private Button BtnSave;
+        private Button BtnCancel;
     }
 }
