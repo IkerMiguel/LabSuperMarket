8e3dc17a8d6cbaa277e9577e9dda8d4ece6b4664
IkerMiguel <ikmiguel03@gmail.com>
2024-10-08 23:42:28 -0500

    Add ICategorieRepository

--- Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845a631f5a40a59064523824a1233c817f3cca8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd59dcfb28186944acbdb59929ac3dd104f4706")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
