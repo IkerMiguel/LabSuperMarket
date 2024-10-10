e126d6274a6cfa9cf1255c91026f560286b15fad
IkerMiguel <ikmiguel03@gmail.com>
2024-10-10 00:06:05 -0500

    Add IProductView

---
+++ Supermarket-mvp/Views/IProductView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface IProductView
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductStock { get; set; }
+        public string ProductCategorie { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetProductListBildingSource(BindingSource productList);
+        void Show();
+    }
+}
